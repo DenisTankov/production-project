be466313b216818ab86a211a713f3cf13f96b926
Denis Tankov <104718229+DenisTankov@users.noreply.github.com>
2024-01-16 23:31:16 +0700

    add feature sliced design

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -6,9 +6,7 @@ import { buildLoaders } from "./buildLoaders";
 import { buildResolvers } from "./buildResolvers";
 import { buildDevServer } from "./buildDevServer";
 
-export function buildWebpackConfig(
-   options: BuildOptions
-): webpack.Configuration {
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
    const { paths, mode, isDev } = options;
    return {
       mode,
@@ -22,7 +20,7 @@ export function buildWebpackConfig(
       module: {
          rules: buildLoaders(options),
       },
-      resolve: buildResolvers(),
+      resolve: buildResolvers(options),
       devtool: isDev ? "inline-source-map" : undefined,
       devServer: isDev ? buildDevServer(options) : undefined,
    };
