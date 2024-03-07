ddb67bd53491c181602452e8b5fd00cd4d8e9d9a
Denis Tankov <104718229+DenisTankov@users.noreply.github.com>
2024-03-07 21:35:18 +0700

    fix

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -27,7 +27,7 @@ export const Clear: Story = {
    },
 };
 
-export const clearInverted: Story = {
+export const ClearInverted: Story = {
    args: {
       children: "Text",
       theme: ThemeButton.CLEAR_INVERTED,
