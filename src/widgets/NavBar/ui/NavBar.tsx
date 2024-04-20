ac2022fb5f8e7298a4e26d9c9358e07e5ca7b42b
Denis Tankov <104718229+DenisTankov@users.noreply.github.com>
2024-04-20 23:30:44 +0700

    add lazy loading, add reducer manager

--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -52,7 +52,7 @@ export const NavBar = ({ className }: NavbarProps) => {
          >
             {t("Войти")}
          </Button>
-         <LoginModal isOpen={isAuthModal} onClose={onCloseModal} />
+         {isAuthModal && <LoginModal isOpen={isAuthModal} onClose={onCloseModal} />}
       </div>
    );
 };
