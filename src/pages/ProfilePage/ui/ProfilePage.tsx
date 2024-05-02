77d8a0fff001c76811c37cb3f110244ea64c5c34
Denis Tankov <104718229+DenisTankov@users.noreply.github.com>
2024-05-02 16:15:17 +0700

    add profile card ui, add strict mode

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,11 +1,12 @@
-import { classNames } from "shared/lib/classNames/classNames";
-import cls from "./ProfilePage.module.scss";
+import { ProfileCard, fetchProfileData, profileReducer } from "entities/Profile";
+import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import { classNames } from "shared/lib/classNames/classNames";
 import {
    DynamicModuleLoader,
    ReducersList,
 } from "shared/lib/components/DynamicModuleLoader/DynamicModuleLoader";
-import { profileReducer } from "entities/Profile";
+import { useAppDispatch } from "shared/lib/hooks/useAppDispatch/useAppDispatch";
 
 const reducers: ReducersList = {
    profile: profileReducer,
@@ -17,10 +18,17 @@ interface ProfilePageProps {
 
 const ProfilePage = ({ className }: ProfilePageProps) => {
    const { t } = useTranslation();
+   const dispatch = useAppDispatch();
+
+   useEffect(() => {
+      dispatch(fetchProfileData());
+   }, [dispatch]);
 
    return (
-      <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
-         <div className={classNames("", {}, [className])}>{t("Страница профиля")}</div>
+      <DynamicModuleLoader reducers={reducers}  removeAfterUnmount>
+         <div className={classNames("", {}, [className])}>
+            <ProfileCard />
+         </div>
       </DynamicModuleLoader>
    );
 };
