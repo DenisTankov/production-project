87b685441b90e90f90e0d541cb02d78008855380
Denis Tankov <104718229+DenisTankov@users.noreply.github.com>
2024-06-12 18:26:54 +0700

    fix3

--- src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -25,7 +25,7 @@ export const Primary: Story = {
          first: "Denis",
          city: "Novosibirsk",
          currency: Currency.RUB,
-         avatar,
+         avatar: "https://pic.rutubelist.ru/user/3b/27/3b2758ad5492a76b578f7ee072e4e894.jpg",
       },
    },
 };
