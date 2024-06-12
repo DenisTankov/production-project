8a98912e43118740714c6101bbf679b99b2a6827
Denis Tankov <104718229+DenisTankov@users.noreply.github.com>
2024-06-12 16:33:47 +0700

    fixed profilecard stories

--- src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Country } from "entities/Country";
 import { Currency } from "entities/Currency";
 import { ProfileCard } from "entities/Profile";
+import avatar from "shared/assets/tests/storybook.jpg";
 
 const meta = {
    title: "entities/ProfileCard",
@@ -24,7 +25,7 @@ export const Primary: Story = {
          first: "Denis",
          city: "Novosibirsk",
          currency: Currency.USD,
-         avatar: "https://pic.rutubelist.ru/user/3b/27/3b2758ad5492a76b578f7ee072e4e894.jpg",
+         avatar,
       },
    },
 };
